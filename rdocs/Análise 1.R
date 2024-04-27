77608f7e17edf2c0e57918713a1daa3aca7b69ee
gahenvi1 <gabriel.henrique.ovidio@gmail.com>
2024-04-27 13:45:11 -0300

    first commit

--- rdocs/Análise 1.R
+++ rdocs/Análise 1.R
@@ -75,7 +75,7 @@ ggplot(teste_frequencias) +
   theme_estat()
 
 #anotações:
-# primeira data registrada é do ano 1969 e ultima data registrada é do ano 2021,  
+# primeira data registrada é do ano 1969 e ultima data registrada é do ano 2021  
 ggsave("resultados/colunas-multi-freq.pdf", width = 230, height = 100, units = "mm")
 
 
@@ -103,3 +103,85 @@ ggplot(ana1, aes(x = decade, y = total)) +
 
 #gráfico lollipop quantidade de episodios por década
 ggsave("resultados/lollipop.pdf", width = 158, height = 93, units = "mm")
+
+
+
+
+# funcao para quadro de medidas resumo da padronizacao
+print_quadro_resumo <- function(data, title="Medidas resumo da(o) [nome da variável]", label="quad:quadro_resumo1")
+{
+  data <- data %>%
+    summarize(`Média` = round(mean(movie),2),
+              `Desvio Padrão` = round(sd(movie),2),
+              `Variância` = round(var(movie),2),
+              `Mínimo` = round(min(movie),2),
+              `1º Quartil` = round(quantile(movie, probs = .25),2),
+              `Mediana` = round(quantile(movie, probs = .5),2),
+              `3º Quartil` = round(quantile(movie, probs = .75),2),
+              `Máximo` = round(max(movie),2)) %>%
+    t() %>% 
+    as.data.frame() %>%
+    rownames_to_column()
+  
+  latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep="")
+  
+  col_count <- ncol(data)
+  row_count <- nrow(data)
+  latex <- str_c(latex, "| l", "|", sep=" ")
+  for (i in seq(2, col_count))
+  {
+    latex <- str_c(latex, "S", sep=" ")
+  }
+  
+  
+  latex <- str_c(latex, "|}\n\t\\toprule\n\t\t", sep="")
+  if (col_count > 2)
+  {
+    for (i in seq(1,col_count))
+    {
+      if (i == 1)
+        latex <- str_c(latex, "\\textbf{Estatística}", sep="")
+      else
+        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
+      
+      if (i < col_count)
+        latex <- str_c(latex, "&", sep=" ")
+      else
+        latex <- str_c(latex, "\\\\\n", sep=" ")
+    }
+  }
+  else
+  {
+    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep="")  
+  }
+  
+  latex <- str_c(latex, "\t\t\\midrule\n", sep="")
+  
+  if (col_count > 2)
+    starting_number <- 2
+  else
+    starting_number <- 1
+  
+  for (i in seq(starting_number, row_count))
+  {
+    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+  }
+  latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep="")
+  
+  writeLines(latex)
+}
+
+ana1 %>%
+  print_quadro_resumo()
+
+
+
+
